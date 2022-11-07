c1f885c049fdae300f0dff57cec5c062ac5e51fa
LeeChiYun <carrick035@gmail.com>
2022-11-07 22:34:23 +0900

    for문으로

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    <Post
+      :인스타데이터="인스타데이터[i]"
+      v-for="(a, i) in 인스타데이터"
+      :key="i"
+    />
+  </div>
+</template>
+<script>
+import Post from "./Post.vue";
+
+export default {
+  components: {
+    Post,
+  },
+  props: {
+    인스타데이터: Array,
+  },
+};
+</script>
+<style lang=""></style>
